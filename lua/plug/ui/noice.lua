819a98a12608ecd24ffdfe92d1b9658677ed3fe6
MuteSTEVE <Dr.MuteSTEVE2@protonmailcom>
2023-11-25 12:35:12 +0700

    "Noice" lazy-load

---
+++ lua/plug/ui/noice.lua
@@ -0,0 +1,80 @@
+local PLUG = {
+  'folke/noice.nvim',
+  dependencies = {
+    'MunifTanjim/nui.nvim',
+    'rcarriga/nvim-notify',
+  },
+  event= { "BufReadPost", "BufNewFile" }
+}
+
+function PLUG.config()
+  local noice_ok, noice = pcall(require, "noice")
+  if not noice_ok then
+    return
+  end
+
+  local notify_ok, notify = pcall(require, 'notify')
+  if not notify_ok then
+    return
+  end
+
+  noice.setup {
+    cmdline = {
+      enabled = true,
+      view = "cmdline_popup",
+      opts = {},
+      format = {
+        cmdline = { pattern = "^:", icon = "", lang = "vim" },
+        search_down = { kind = "search", pattern = "^/", icon = "󰍉", lang = "regex" },
+        search_up = { kind = "search", pattern = "^%?", icon = " ", lang = "regex" },
+        filter = { pattern = "^:%s*!", icon = " ", lang = "bash" },
+        lua = { pattern = { "^:%s*lua%s+", "^:%s*lua%s*=%s*", "^:%s*=%s*" }, icon = "", lang = "lua" },
+        help = { pattern = "^:%s*he?l?p?%s+", icon = "󰋖" },
+        input = {},
+      },
+    },
+    views = {
+      cmdline_popup = {
+        position = {
+          row = 5,
+          col = "50%",
+        },
+        size = {
+          width = 60,
+          height = "auto",
+        },
+      },
+      popupmenu = {
+        relative = "editor",
+        position = {
+          row = 8,
+          col = "50%",
+        },
+        size = {
+          width = 60,
+          height = 10,
+        },
+      }
+    }
+  }
+
+  notify.setup{
+    background_colour = "Normal",
+    fps = 30,
+    icons = {
+      DEBUG = "",
+      ERROR = "",
+      INFO = "",
+      TRACE = "✎",
+      WARN = ""
+    },
+    level = 2,
+    minimum_width = 50,
+    maximum_width = 50,
+    render = "default",
+    stages = "slide",
+    timeout = 1000
+  }
+end
+
+return PLUG
